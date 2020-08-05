b03f77c42e6bb1f16c572be66f3073f0f7d99db3
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-05 02:15:31 -0400

    comments for steping through each cell

--- index.js
+++ index.js
@@ -62,4 +62,19 @@ const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
 
-console.log(grid);
+//* Random starting point
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+//* Go through each cell
+const stepThroughCell = (row, column) => {
+  //* If I have visited the cell at [row, column], then return
+  //* Mark cells that have been visited
+  //* Assemble randomly-ordered list of neighbors
+  //* For each neighbor
+  //* See if that neighbor is out of bounds
+  //* If we have visited that neighbor, continue to next neighbor
+  //* Visit that next cell
+};
+
+stepThroughCell(startRow, startColumn);
