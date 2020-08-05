5c4e70267bc49acc9c5b2fa1984fd1daf80382d4
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-05 01:40:32 -0400

    refractor for project

--- index.js
+++ index.js
@@ -3,17 +3,9 @@
 //* Runner - Coordinate the Engine and World
 //* World - Snapshot of the world
 //* Bodies - Geometry in the world
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-  MouseConstraint,
-  Mouse,
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 //* Create New Engine
@@ -35,69 +27,24 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-//* Allows shapes to be grab
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-    constraint: {
-      render: {
-        visible: false,
-      },
-    },
-  })
-);
-
 //* Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, {
+  Bodies.rectangle(width / 2, 0, width, 40, {
     isStatic: true,
-    render: {
-      visible: false,
-    },
   }),
-  Bodies.rectangle(400, 600, 800, 40, {
+  Bodies.rectangle(width / 2, height, width, 40, {
     isStatic: true,
-    render: {
-      visible: false,
-    },
   }),
-  Bodies.rectangle(0, 300, 40, 600, {
+  Bodies.rectangle(0, height / 2, 40, height, {
     isStatic: true,
-    render: {
-      visible: false,
-    },
   }),
-  Bodies.rectangle(800, 300, 40, 600, {
+  Bodies.rectangle(width, height / 2, 40, height, {
     isStatic: true,
-    render: {
-      visible: false,
-    },
   }),
 ];
 
 //* Add Walls to the world
 World.add(world, walls);
-
-//* Random Shapes
-for (let i = 0; i < 50; i++) {
-  if (Math.random() >= 0.7) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else if (Math.random() < 0.7 && Math.random() >= 0.3) {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 25)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.trapezoid(Math.random() * width, Math.random() * height, 50, 50, 2)
-    );
-  }
-}
 
 //* When objects leave the canvas
 Matter.World.remove(world, body);
