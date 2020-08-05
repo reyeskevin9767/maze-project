7ca7792301ef80ab2ad8df4189865d9a0c09f517
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-05 03:38:58 -0400

    add horizonatal walls

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 //* Create New Engine
 const engine = Engine.create();
 const { world } = engine;
@@ -129,9 +131,32 @@ const stepThroughCell = (row, column) => {
     } else if (direction === 'down') {
       horizontals[row][column] = true;
     }
-  }
 
-  // Visit the next cell
+    // Visit the next cell
+    stepThroughCell(nextRow, nextColumn);
+  }
 };
 
+//* Starting point to create maze
 stepThroughCell(startRow, startColumn);
+
+//* Draw horizontal walls
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open === true) {
+      return;
+    }
+
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      {
+        isStatic: true,
+      }
+    );
+
+    World.add(world, wall);
+  });
+});
